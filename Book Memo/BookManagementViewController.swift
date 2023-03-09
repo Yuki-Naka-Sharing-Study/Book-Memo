c6ed0adc77fbfdc414496a77e7dbaf74d3859348
Yuki Naka <48172941+Yuki-Naka-Sharing-Study@users.noreply.github.com>
2023-03-09 14:37:35 +0900

    「BookManagementController」 「BookRegisterController」 「OtherViewController」
    を登録。

---
+++ Book Memo/BookManagementViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookManagementViewController.swift
+//  Book Memo
+//
+//  Created by 仲優樹 on 2023/03/09.
+//
+
+import UIKit
+
+class BookManagementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
