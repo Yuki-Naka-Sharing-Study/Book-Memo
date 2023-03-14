08e4a3d166ea3e234f96ec961fc1878d685de02a
Yuki Naka <48172941+Yuki-Naka-Sharing-Study@users.noreply.github.com>
2023-03-14 14:16:00 +0900

    「Firebase」を使っていないので「import FirebaseStorageUI」を削除。

--- Book Memo/BookImageTableViewCell.swift
+++ Book Memo/BookImageTableViewCell.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import FirebaseStorageUI
 
 class BookImageTableViewCell: UITableViewCell {
     
@@ -21,14 +20,6 @@ class BookImageTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         // Configure the view for the selected state
-    }
-    
-    // PostDataの内容をセルに表示
-    func setPostData(_ postData: Book) {
-        // 画像の表示
-        BookImageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
-        let imageRef = Storage.storage().reference().child(Const.ImagePath).child(postData.id + ".jpg")
-        BookImageView.sd_setImage(with: imageRef)
     }
     
 }
