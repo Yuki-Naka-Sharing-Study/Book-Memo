fe306d407200dc654e93640c122ef28f220489d1
Yuki Naka <48172941+Yuki-Naka-Sharing-Study@users.noreply.github.com>
2023-03-17 22:34:16 +0900

    「BookRegisterViewController」のUI完成 (仮)

--- Book Memo/ViewController/BookRegisterViewController.swift
+++ Book Memo/ViewController/BookRegisterViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CLImageEditor
 
-class BookRegisterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class BookRegisterViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLImageEditorDelegate {
     
     @IBOutlet weak var bookTitleTextField: UITextField?
     @IBOutlet weak var bookReviewTextView: UITextView?
@@ -21,7 +21,7 @@ class BookRegisterViewController: UIViewController, UIImagePickerControllerDeleg
     
     
     
-    @IBAction func register(_ sender: Any) {
+    @IBAction func getImage(_ sender: Any) {
         // カメラを指定してピッカーを開く
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             let pickerController = UIImagePickerController()
@@ -31,50 +31,51 @@ class BookRegisterViewController: UIViewController, UIImagePickerControllerDeleg
         }
     }
     
-    // 写真を撮影/選択したときに呼ばれるメソッド
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//        // UIImagePickerController画面を閉じる
-//        picker.dismiss(animated: true, completion: nil)
-//        // 画像加工処理
-//        if info[.originalImage] != nil {
-//            // 撮影/選択された画像を取得する
-//            let image = info[.originalImage] as! UIImage
-//
-//            // CLImageEditorライブラリで加工する
-//            print("DEBUG_PRINT: image = \(image)")
-//            // CLImageEditorにimageを渡して、加工画面を起動する。
-//            let editor = CLImageEditor(image: image)!
-//            editor.delegate = self
-//            self.present(editor, animated: true, completion: nil)
-//        }
-//    }
+    @IBAction func registerBook(_ sender: Any) {
+        
+    }
     
-//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-//        // UIImagePickerController画面を閉じる
-//        picker.dismiss(animated: true, completion: nil)
-//    }
+//     写真を撮影/選択したときに呼ばれるメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // UIImagePickerController画面を閉じる
+        picker.dismiss(animated: true, completion: nil)
+        // 画像加工処理
+        if info[.originalImage] != nil {
+            // 撮影/選択された画像を取得する
+            let image = info[.originalImage] as! UIImage
 
-    // CLImageEditorで加工が終わったときに呼ばれるメソッド
-//    func imageEditor(_ editor: CLImageEditor!, didFinishEditingWith image: UIImage!) {
-//        // 投稿画面を開く
-//        let bookManagementViewController = self.storyboard?.instantiateViewController(withIdentifier: "Post") as! BookManagementViewController
-//        bookManagementViewController.image = image!
-//        editor.present(bookManagementViewController, animated: true, completion: nil)
-//    }
+            // CLImageEditorライブラリで加工する
+            print("DEBUG_PRINT: image = \(image)")
+            // CLImageEditorにimageを渡して、加工画面を起動する。
+            let editor = CLImageEditor(image: image)!
+            editor.delegate = self
+            self.present(editor, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // UIImagePickerController画面を閉じる
+        picker.dismiss(animated: true, completion: nil)
+    }
 
-    // CLImageEditorの編集がキャンセルされた時に呼ばれるメソッド
-//    func imageEditorDidCancel(_ editor: CLImageEditor!) {
-//        // CLImageEditor画面を閉じる
-//        editor.dismiss(animated: true, completion: nil)
-//    }
+//     CLImageEditorで加工が終わったときに呼ばれるメソッド
+    func imageEditor(_ editor: CLImageEditor!, didFinishEditingWith image: UIImage!) {
+        // 投稿画面を開く imageViewに画像を渡す
+    }
+
+//     CLImageEditorの編集がキャンセルされた時に呼ばれるメソッド
+    func imageEditorDidCancel(_ editor: CLImageEditor!) {
+        // CLImageEditor画面を閉じる
+        editor.dismiss(animated: true, completion: nil)
+    }
     
     /*
-    // MARK: - Navigation
+     MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+         Get the new view controller using segue.destination.
+         Pass the selected object to the new view controller.
     }
     */
 
