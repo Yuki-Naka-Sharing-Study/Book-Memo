4eced41f78b17abcda6f04183c314803e78fc892
Yuki Naka <48172941+Yuki-Naka-Sharing-Study@users.noreply.github.com>
2023-03-27 18:51:22 +0900

    【エラー発生】「Instance member 'book' cannot be used on type 'BookDisplayViewController'; did you mean to use a value of this type instead?」

--- Book Memo/ViewController/BookManagementViewController.swift
+++ Book Memo/ViewController/BookManagementViewController.swift
@@ -34,6 +34,8 @@ class BookManagementViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
     }
+    
+    
 }
 
 extension BookManagementViewController: UITableViewDelegate, UITableViewDataSource {
@@ -54,6 +56,15 @@ extension BookManagementViewController: UITableViewDelegate, UITableViewDataSour
     // 各セルを選択した時に実行されるメソッド
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "cellSegue",sender: nil)
+        
+        let book = Book()
+        
+        let allBooks = realm.objects(Book.self)
+        if allBooks.count != 0 {
+            book.id = allBooks.max(ofProperty: "id")! + 1
+        }
+        
+        BookDisplayViewController.book = book
     }
     
     // セルが削除が可能なことを伝えるメソッド
